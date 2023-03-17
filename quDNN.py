449980cefd1467287ff26920f840d038848517b7
MarkCodering <mark.chen.sstm@gmail.com>
2023-03-17 10:04:44 +0800

    update code descriptions

---
+++ quDNN.py
@@ -0,0 +1,335 @@
+
+import numpy as np
+import scipy.special as sp
+
+from IPython.display import display, clear_output
+import matplotlib.pyplot as plt
+import copy
+import time
+import random
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import collections
+
+
+# **Introduce experience replay.**
+
+
+Transition = collections.namedtuple('Experience',
+                                    field_names=['state', 'action',
+                                                 'next_state', 'reward',
+                                                 'is_game_on'])
+
+class ExperienceReplay:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, transition):
+        self.memory.append(transition)
+
+    def sample(self, batch_size, device = 'cuda'):
+        indices = np.random.choice(len(self.memory), batch_size, replace = False)
+        
+        states, actions, next_states, rewards, isgameon = zip(*[self.memory[idx] 
+                                                                for idx in indices])
+        
+        return torch.Tensor(states).type(torch.float).to(device), \
+               torch.Tensor(actions).type(torch.long).to(device), \
+               torch.Tensor(next_states).to(device), \
+               torch.Tensor(rewards).to(device), torch.tensor(isgameon).to(device)
+
+# **Networks definition.**
+
+# Necessary imports
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torch import Tensor
+from torch.nn import Linear, CrossEntropyLoss, MSELoss
+from torch.optim import LBFGS
+
+from qiskit import QuantumCircuit
+from qiskit.utils import algorithm_globals
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import RealAmplitudes, ZZFeatureMap, ZFeatureMap
+from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
+from qiskit_machine_learning.algorithms.regressors import NeuralNetworkRegressor, VQR
+from qiskit_machine_learning.connectors import TorchConnector
+from qiskit_machine_learning.neural_networks import NeuralNetwork
+from torch import cat
+
+# Set seed for random generators
+algorithm_globals.random_seed = 42
+
+feature_map = ZZFeatureMap(2)
+ansatz = RealAmplitudes(2, reps=1)
+qc = QuantumCircuit(2)
+qc.compose(feature_map, inplace=True)
+qc.compose(ansatz, inplace=True)
+
+# REMEMBER TO SET input_gradients=True FOR ENABLING HYBRID GRADIENT BACKPROP
+qnn = EstimatorQNN(
+    circuit=qc,
+    input_params=feature_map.parameters,
+    weight_params=ansatz.parameters,
+    input_gradients=True,
+)
+
+class fc_nn(nn.Module):
+    def __init__(self, Ni, Nh1, Nh2, No = 4, qnn = qnn):
+        super().__init__()
+        
+        self.fc1 = nn.Linear(Ni, Nh1)
+        self.fc2 = nn.Linear(Nh1, Nh2)
+        self.fc3 = Linear(400, 200)
+        self.fc4 = Linear(200, 2) 
+        self.qnn = TorchConnector(qnn)
+        self.fcl = Linear(1, 1)
+        self.fc5 = Linear(1, 4) 
+        self.act = nn.ReLU()
+        
+    def forward(self, x, classification = False, additional_out=False):
+        x = self.act(self.fc1(x))
+        x = self.act(self.fc2(x))
+        x = self.fc3(x)
+        x = self.fc4(x)
+        x = self.qnn(x)
+        #x = torch.tensor(x)
+        x = self.fcl(x)
+        # Convert to torch tensor
+        x = self.fc5(x)
+
+        return x 
+
+class conv_nn(nn.Module):
+    
+    channels = [16, 32, 64]
+    kernels = [3, 3, 3]
+    strides = [1, 1, 1]
+    in_channels = 1
+    
+    def __init__(self, rows, cols, n_act):
+        super().__init__()
+        self.rows = rows
+        self.cols = cols
+
+        self.conv = nn.Sequential(nn.Conv2d(in_channels = self.in_channels,
+                                            out_channels = self.channels[0],
+                                            kernel_size = self.kernels[0],
+                                            stride = self.strides[0]),
+                                  nn.ReLU(),
+                                  nn.Conv2d(in_channels = self.channels[0],
+                                            out_channels = self.channels[1],
+                                            kernel_size = self.kernels[1],
+                                            stride = self.strides[1]),
+                                  nn.ReLU()
+                                 )
+
+        
+        size_out_conv = self.get_conv_size(rows, cols)
+        
+        self.linear = nn.Sequential(nn.Linear(size_out_conv, rows*cols*2),
+                                    nn.ReLU(),
+                                    nn.Linear(rows*cols*2, int(rows*cols/2)),
+                                    nn.ReLU(),
+                                    nn.Linear(int(rows*cols/2), n_act),
+                                   )
+
+    def forward(self, x):
+        x = x.view(len(x), self.in_channels, self.rows, self.cols)
+        out_conv = self.conv(x).view(len(x),-1)
+        out_lin = self.linear(out_conv)
+        return out_lin
+    
+    def get_conv_size(self, x, y):
+        out_conv = self.conv(torch.zeros(1,self.in_channels, x, y))
+        return int(np.prod(out_conv.size()))
+
+
+def Qloss(batch, net, gamma=0.99, device="cuda"):
+    states, actions, next_states, rewards, _ = batch
+    lbatch = len(states)
+    state_action_values = net(states.view(lbatch,-1))
+    state_action_values = state_action_values.gather(1, actions.unsqueeze(-1))
+    state_action_values = state_action_values.squeeze(-1)
+    #state_action_values = torch.tensor(input_list).squeeze()
+    
+    next_state_values = net(next_states.view(lbatch, -1))
+    next_state_values = next_state_values.max(1)[0]
+    
+    next_state_values = next_state_values.detach()
+    expected_state_action_values = next_state_values * gamma + rewards
+    
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+
+# **Import the maze and define the environment.**
+
+
+from environment import MazeEnvironment
+
+maze = np.load('maze_generator/maze.npy')
+
+initial_position = [0,0]
+goal = [len(maze)-1, len(maze)-1]
+
+maze_env = MazeEnvironment(maze, initial_position, goal)
+
+
+maze_env.draw('maze_20.pdf')
+
+
+# **Define the agent and the buffer for experience replay.**
+
+
+buffer_capacity = 10000
+buffer_start_size = 1000
+memory_buffer = ExperienceReplay(buffer_capacity)
+
+
+from agent import Agent
+agent = Agent(maze = maze_env,
+              memory_buffer = memory_buffer,
+              use_softmax = True
+             )
+
+
+net = fc_nn(maze.size, maze.size, maze.size, 4, qnn)
+optimizer = optim.Adam(net.parameters(), lr=1e-4)
+
+
+device = 'cuda'
+batch_size = 24
+gamma = 0.9
+
+net.to(device)
+
+# **Define the epsilon profile and plot the resetting probability.**
+
+num_epochs = 20000
+
+cutoff = 3000
+epsilon = np.exp(-np.arange(num_epochs)/(cutoff))
+epsilon[epsilon > epsilon[100*int(num_epochs/cutoff)]] = epsilon[100*int(num_epochs/cutoff)]
+plt.plot(epsilon, color = 'orangered', ls = '--')
+plt.xlabel('Epochs')
+plt.ylabel('Epsilon')
+plt.savefig('epsilon_profile.pdf', dpi = 300, bbox_inches = 'tight')
+plt.show()
+
+mp = []
+mpm = []
+reg = 200
+for e in epsilon:
+    a = agent.env.reset_policy(e)
+    mp.append(np.min(a))
+    mpm.append(np.max(a))
+
+plt.plot(epsilon/1.3, color = 'orangered', ls = '--', alpha = 0.5,
+         label= 'Epsilon profile (arbitrary units)')
+
+plt.plot(np.array(mpm)-np.array(mp), label = 'Probability difference', color = 'cornflowerblue')
+plt.xlabel('Epochs')
+plt.ylabel(r'max $p^r$ - min $p^r$')
+plt.legend()
+plt.savefig('reset_policy.pdf', dpi = 300, bbox_inches = 'tight')
+plt.show()
+
+# **Training the network.**
+
+loss_log = []
+best_loss = 1e5
+
+running_loss = 0
+
+for epoch in range(num_epochs):
+    loss = 0
+    counter = 0
+    eps = epsilon[epoch]
+    
+    agent.isgameon = True
+    _ = agent.env.reset(eps)
+    
+    while agent.isgameon:
+        agent.make_a_move(net, eps)
+        counter += 1
+        
+        if len(agent.buffer) < buffer_start_size:
+            continue
+            
+        optimizer.zero_grad()
+        batch = agent.buffer.sample(batch_size, device = device)
+        loss_t = Qloss(batch, net, gamma = gamma, device = device)
+        loss_t.backward()
+        optimizer.step()
+        
+        loss += loss_t.item()
+    
+    if (agent.env.current_position == agent.env.goal).all():
+        result = 'won'
+    else:
+        result = 'lost'
+    
+    if epoch%1000 == 0:
+        agent.plot_policy_map(net, 'sol_epoch_'+str(epoch)+'.pdf', [0.35,-0.3])
+    
+    loss_log.append(loss)
+    
+    if (epoch > 2000):
+        running_loss = np.mean(loss_log[-50:])
+        if running_loss < best_loss:
+            best_loss = running_loss
+            torch.save(net.state_dict(), "best.torch")
+            estop = epoch
+    
+    print('Epoch', epoch, '(number of moves ' + str(counter) + ')')
+    print('Game', result)
+    print('[' + '#'*(100-int(100*(1 - epoch/num_epochs))) +
+          ' '*int(100*(1 - epoch/num_epochs)) + ']')
+    print('\t Average loss: ' + f'{loss:.5f}')
+    if (epoch > 2000):
+        print('\t Best average loss of the last 50 epochs: ' + f'{best_loss:.5f}' + ', achieved at epoch', estop)
+    clear_output(wait = True)
+
+torch.save(net.state_dict(), "net.torch")
+
+
+plt.plot(epsilon*90, alpha = 0.6, ls = '--', label = 'Epsilon profile (arbitrary unit)', color = 'orangered')
+plt.plot((np.array(mpm)-np.array(mp))*120, alpha = 0.6, ls = '--',
+         label = 'Probability difference (arbitrary unit)', color = 'dimgray')
+plt.plot(loss_log, label = 'Loss', color = 'cornflowerblue')
+plt.xlabel('Epoch')
+plt.ylabel('MSE')
+plt.legend()
+plt.savefig('loss.pdf', dpi = 300, bbox_inches='tight')
+plt.show()
+
+# **Show the maze solution and the policy learnt.**
+
+net.eval()
+agent.isgameon = True
+agent.use_softmax = False
+_ = agent.env.reset(0)
+while agent.isgameon:
+    agent.make_a_move(net, 0)
+    agent.env.draw('')
+    clear_output(wait = True)
+
+
+agent.plot_policy_map(net, 'solution.pdf', [0.35,-0.3])
+
+
+best_net = copy.deepcopy(net)
+best_net.load_state_dict(torch.load('best.torch'))
+
+
+agent.plot_policy_map(best_net, 'solution_best.pdf', [0.35,-0.3])
+
+
